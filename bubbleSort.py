515fd4add8324a6c7c8d5f5f3576cfe2b2d763de
Sumin0411 <kimsumin411@gmail.com>
2023-11-09 13:08:48 +0900

    Third

--- bubbleSort.py
+++ bubbleSort.py
@@ -3,3 +3,16 @@ def bubble_sort(arr):
         for j in range(i):
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
+
+
+
+#최적화 : 이전에 swap이 한번도 안 일어남
+def bubble_sort(arr):
+    for i in range(len(arr) - 1, 0, -1):
+        swapped = False
+        for j in range(i):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                swapped = True
+            if not swapped:
+                break
